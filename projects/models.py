dd6ee0ab343c6b4f558196f2a0fcc0543e4e3fe2
Oswaldo <oswalfut_96@hotmail.com>
2016-12-20 13:10:20 -0600

    tudo bem

--- projects/models.py
+++ projects/models.py
@@ -2,11 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Project(models.Model):
-    name = models.CharField(max_length=250)
+    name = models.CharField(max_length=250, blank=True, null=True)
     desc = models.TextField(blank=True, null=True)
-    user = models.ForeignKey(User, related_name="projects")
-    goal = models.CharField(max_length=140,default=1)
-    publish = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, related_name="projects", blank=True, null=True)
+    goal = models.CharField(max_length=140,default=1, blank=True, null=True)
+    publish = models.DateTimeField(auto_now=True, blank=True, null=True)
     img = models.ImageField(upload_to="projects/images", blank=True, null=True)
 
 
@@ -26,5 +26,5 @@ class Image(models.Model):
     project = models.ForeignKey(Project,related_name="images")
 
 class NewProject(models.Model):
-    name = models.CharField(max_length=250)
-    goal = models.DecimalField(max_digits=7, decimal_places=2, default=1)
+    name = models.CharField(max_length=250, blank=True, null=True)
+    goal = models.DecimalField(max_digits=7, decimal_places=2, default=1, blank=True, null=True)
