c6642328084a97a9226850dd318c8fd1955a4447
BlisS <contacto@fixter.org>
2016-12-27 23:28:49 -0600

    working on explorar

--- projects/admin.py
+++ projects/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Project
+from .models import Project, Reward
 
 from .models import *
 # Register your models here.
 
 
 admin.site.register(Project)
+admin.site.register(Reward)
